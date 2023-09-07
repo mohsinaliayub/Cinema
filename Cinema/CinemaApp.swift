88db9da44a62de6ff07025b390c68bc201835213
Mohsin Ali Ayub <meismosin@gmail.com>
2023-09-07 10:57:42 +0200

    Initial Commit

---
+++ Cinema/CinemaApp.swift
@@ -0,0 +1,17 @@
+//
+//  CinemaApp.swift
+//  Cinema
+//
+//  Created by Mohsin Ali Ayub on 07.09.23.
+//
+
+import SwiftUI
+
+@main
+struct CinemaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
